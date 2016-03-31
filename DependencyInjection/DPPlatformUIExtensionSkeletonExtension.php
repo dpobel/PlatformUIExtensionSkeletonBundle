3ccc71cbea54c96f0742aa56106d1a8fd06e46da
Damien Pobel <dp@ez.no>
2016-03-31 17:41:39 +0200

    Prepared the bundle to be a PlatformUI extension

--- DependencyInjection/DPPlatformUIExtensionSkeletonExtension.php
+++ DependencyInjection/DPPlatformUIExtensionSkeletonExtension.php
@@ -7,12 +7,18 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ContextualizerInterface;
+use Symfony\Component\Config\Resource\FileResource;
+
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DPPlatformUIExtensionSkeletonExtension extends Extension
+class DPPlatformUIExtensionSkeletonExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +30,41 @@ class DPPlatformUIExtensionSkeletonExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $processor = new ConfigurationProcessor($container, 'dp_platform_ui_extension_skeleton');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig(
+            'assetic', array('bundles' => array('DPPlatformUIExtensionSkeletonBundle'))
+        );
+
+        $this->prependYui( $container );
+        $this->prependCss( $container );
+    }
+
+    private function prependYui(ContainerBuilder $container)
+    {
+        $container->setParameter(
+            'extension_skeleton.public_dir',
+            'bundles/dpplatformuiextensionskeleton'
+        );
+        $yuiConfigFile = __DIR__ . '/../Resources/config/yui.yml';
+        $config = Yaml::parse(file_get_contents($yuiConfigFile));
+        $container->prependExtensionConfig('ez_platformui', $config);
+        $container->addResource(new FileResource($yuiConfigFile));
+    }
+
+    private function prependCss(ContainerBuilder $container)
+    {
+        $container->setParameter(
+            'extension_skeleton.css_dir',
+            'bundles/dpplatformuiextensionskeleton/css'
+        );
+        $cssConfigFile = __DIR__ . '/../Resources/config/css.yml';
+        $config = Yaml::parse(file_get_contents($cssConfigFile));
+        $container->prependExtensionConfig('ez_platformui', $config);
+        $container->addResource(new FileResource($cssConfigFile));
     }
 }
