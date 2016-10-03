88258611cd5ae312e1dde97b00379906492f29f7
Damien Pobel <dp@ez.no>
2016-10-03 11:24:03 +0200

    Added the sign count plugin to RichText field edit view

--- Resources/public/js/views/fields/plugins/dp-signcounterplugin.js
+++ Resources/public/js/views/fields/plugins/dp-signcounterplugin.js
@@ -4,20 +4,35 @@ YUI.add('dp-signcounterplugin', function (Y) {
 
     Y.DP.Plugin.SignCounter = Y.Base.create('dpSignCounterPlugin', Y.Plugin.Base, [], {
         initializer: function () {
-            this.onceAfterHostEvent('activeChange', this._setupCounter);
+            var fieldDef = this.get('host').get('fieldDefinition');
+
+            if ( fieldDef.fieldType === 'eztext' ) {
+                this.onceAfterHostEvent('activeChange', this._setupTextBlockCounter);
+            } else if ( fieldDef.fieldType === 'ezrichtext' ) {
+                this.afterHostEvent('instanceReady', this._setupRichTextCounter);
+            }
         },
 
-        _setupCounter: function () {
+        _setupTextBlockCounter: function () {
             var view = this.get('host'),
                 events = Y.merge(view.events, {
                     '.ez-textblock-input-ui textarea': {
-                        'valuechange': Y.bind(this._updateCounter, this),
+                        'valuechange': Y.bind(this._updateTextBlockCounter, this),
                     }
                 });
 
             this._addCounterPlaceHolder();
-            this._updateCounter();
+            this._updateTextBlockCounter();
             view.attachEvents(events);
+        },
+
+        _setupRichTextCounter: function () {
+            this._addCounterPlaceHolder();
+            this._updateRichTextCounter();
+            this.get('host').get('editor').get('nativeEditor').on(
+                'change',
+                Y.bind(this._updateRichTextCounter, this)
+            );
         },
 
         _addCounterPlaceHolder: function () {
@@ -29,18 +44,35 @@ YUI.add('dp-signcounterplugin', function (Y) {
             );
         },
 
-        _count: function () {
+        _countRichText: function () {
+            // this is buggy if an embed is added
+            var data = this.get('host').get('editor').get('nativeEditor').getData(),
+                tmpNode = Y.Node.create('<div />');
+
+            tmpNode.append(data);
+            return tmpNode.get('textContent').length;
+        },
+
+        _countTextBlock: function () {
             return this.get('host').get('container').one('textarea').get('value').length;
         },
 
-        _updateCounter: function () {
-            this.get('host').get('container').one('.dp-signcounter').setContent(this._count());
+        _updateRichTextCounter: function () {
+            this._updateCounter(this._countRichText());
+        },
+
+        _updateTextBlockCounter: function () {
+            this._updateCounter(this._countTextBlock());
+        },
+
+        _updateCounter: function (value) {
+            this.get('host').get('container').one('.dp-signcounter').setContent(value);
         },
     }, {
         NS: 'dpSignCounterPlugin'
     });
 
     Y.eZ.PluginRegistry.registerPlugin(
-        Y.DP.Plugin.SignCounter, ['textBlockEditView']
+        Y.DP.Plugin.SignCounter, ['textBlockEditView', 'richTextEditView']
     );
 });
